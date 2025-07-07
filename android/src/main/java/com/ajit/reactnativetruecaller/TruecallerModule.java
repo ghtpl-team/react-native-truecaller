6607757addc97aca22ad3104fe9460e473d5f51a
veeresh-lokal <veeresh@getlokalapp.com>
2025-07-07 16:55:48 +0530

    fix: add catch block which check for result in ActvityOnResult

--- android/src/main/java/com/ajit/reactnativetruecaller/TruecallerModule.java
+++ android/src/main/java/com/ajit/reactnativetruecaller/TruecallerModule.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.FragmentActivity;
 import com.facebook.react.bridge.*;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.truecaller.android.sdk.oAuth.*;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
@@ -153,7 +154,11 @@ private TcSdkOptions buildSdkOptions(ReadableMap config) {
         public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
             super.onActivityResult(activity, requestCode, resultCode, data);
             if (requestCode == TcSdk.SHARE_PROFILE_REQUEST_CODE) {
-                TcSdk.getInstance().onActivityResultObtained((FragmentActivity) activity, requestCode, resultCode, data);
+                try {
+                    TcSdk.getInstance().onActivityResultObtained((FragmentActivity) activity, requestCode, resultCode, data);
+                } catch (Exception e) {
+                    FirebaseCrashlytics.getInstance().recordException(e);
+                }
             }
         }
     };
