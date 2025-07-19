66696c7e044b2359efc4ec62f702d4d0691eceac
Arun Lodhi <arunlodhi@live.com>
2025-07-20 01:52:09 +0530

    feat: Add type declarations

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for @ajitpatel28/react-native-truecaller
+// Project: https://github.com/ajitpatel28/react-native-truecaller
+// Definitions by: Ajit Patel <https://github.com/ajitpatel28>
+
+export * from './src/index';
